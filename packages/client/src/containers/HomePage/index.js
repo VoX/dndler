eb26b94e65855c30ae33c4f5478c172bf42e361f
unknown <peichman14@gmail.com>
2022-01-27 14:16:27 -0500

    set up basic api call to localhost:8000

--- packages/client/src/containers/HomePage/index.js
+++ packages/client/src/containers/HomePage/index.js
@@ -2,25 +2,47 @@ import React, {useState} from 'react';
 
 const HomePage = () => {
   // Instantiate a local state that can be overwritten via setState
+  // Not everything needs to be kept in state, but it is useful to preserve values between function calls.
+  // Source Doc: https://reactjs.org/docs/hooks-state.html
   const [state, setState] = useState({
     names: ['Brend', 'Brenda', 'Brendle', 'Brendly', 'Brendan', 'Brendaniel', 'Brendanahan', 'Sir Brend of Ann', 'Brendnificient', 'Brendinglydang von Brendlydong'],
-    randomName: ''
+    randomName: '',
+    response: ''
   })
 
-  // Pick name function
+  fetch('http://localhost:8000/', {
+
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  },
+  ).then(response => {
+    console.log(response)
+  })
+  .catch(() => {
+    console.log("Sorry, it borked");
+  });
+
+  // Instantiate pickName function, any arguments you need to pass in can go in the ().
   const pickName = () => {
     const newRandomName = state.names[Math.floor(Math.random() * state.names.length)];
-    // Set state
+    // vv Set the state vv
     setState({
-      // Keeps all previous state
+      // vv Keeps all previous state vv
       ...state,
-      // Overwrites this one part of previous state
+      // vv Overwrites this one part of previous state vv
       randomName: newRandomName
     });
   }
 
+  
+
 
   return (
+    // When returning JSX in React, it must all be wrapped in a single parent element. You can't return two separate parent elements.
+    // React.Fragment allows you to wrap everything in a single parent element that has no inherent properties.
+    // This allows you to get away from wrapping everything in an unstyled <div>, which can be annoying and clutters up the HTML in the inspect tool.
     <React.Fragment>
       <div style={{justifyContent: 'center', display: 'flex'}}>
         <button onClick={pickName}>
