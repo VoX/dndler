c56e2a76cb03b8a2416791a3c61c9b5dc3d27b8a
Varun Nadgir <theminivann@gmail.com>
2022-01-31 13:12:49 -0500

    Update jsonToCsv.py

--- packages/data/jsonToCsv.py
+++ packages/data/jsonToCsv.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import json
 
+# read in json file as dict
 with open('backgrounds.json') as json_file:
     backgrounds = json.load(json_file)
 
-backgrounds_dict = json.loads(backgrounds)
+# turn dict into df
+backgrounds_df = pd.DataFrame.from_dict(backgrounds, 'index')
+
+# write df to csv
+backgrounds_df.to_csv('test.csv')
