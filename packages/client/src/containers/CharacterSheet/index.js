2a7971e59f3bbd58b90a8bd676ae804c6bc09911
br-ndt <nlax76@gmail.com>
2022-02-06 15:27:01 -0500

    initialize character history

--- packages/client/src/containers/CharacterSheet/index.js
+++ packages/client/src/containers/CharacterSheet/index.js
@@ -8,10 +8,13 @@ import Equipment from "./Equipment"
 import SavingThrows from "./SavingThrows"
 import Features from "./CharacterFeatures"
 import OptionButton from "../EventCallers/OptionButton";
+import SaveIcon from "../../img/save.png"
+import LoadIcon from "../../img/load.png"
 
-const CharacterSheet = (props) =>
+const CharacterSheet = () =>
 {
-    const [character, changeCharacter] = useState(null);
+    const [history, setHistory] = useState([]);
+    const [characterIndex, setCharacterIndex] = useState(0);
 
     const fetchCharacter = () =>
     {
@@ -23,73 +26,138 @@ const CharacterSheet = (props) =>
         })
         .then(response => response.json())
         .then((json) => {
-            changeCharacter(json);
+            setHistory([...history, json]);
+            setCharacterIndex(history.length);
         })
         .catch((error) => {
             console.log(`Sorry, it borked cuz ${error}`);
         });
+
     }
 
     useEffect(() => {
         fetchCharacter();
     }, []);
 
-    if(character)
+    const goBackChar = () =>
     {
-        console.log(character);
+        if(characterIndex > 0)
+        {
+            setCharacterIndex(characterIndex - 1);
+        }
+    }
+
+    const goForwardChar = () =>
+    {
+        if(characterIndex < history.length - 1)
+        {
+            setCharacterIndex(characterIndex + 1);
+        }
+    }
+
+    const determineBackButton = () =>
+    {
+        if(characterIndex > 0 && history.length > 1)
+        {
+            return (
+                <OptionButton
+                    label="<<"
+                    onClick={goBackChar}
+                    value={""}
+                    id={"charBack"}
+                    containerName={"inline-block"}
+                    className={"charBack"}
+                />
+            );
+        }
+        else
+        {
+            return <div></div>;
+        }
+    }
+
+    const determineForwardButton = () =>
+    {
+        if(characterIndex + 1 < history.length && history.length > 1)
+        {
+            return (
+                <OptionButton
+                    label=">>"
+                    onClick={goForwardChar}
+                    value={""}
+                    id={"charFrwd"}
+                    containerName={"inline-block"}
+                    className={"charFrwd"}
+                />
+            );
+        }
+        else
+        {
+            return <div></div>;
+        }
+    }
+
+    if(history[characterIndex])
+    {
         return (
             <React.Fragment>
-                <OptionButton
-                    label={"GIMME ANUDDER MIN!"}
-                    onClick={fetchCharacter}
-                    value={""}
-                    id={"reroll"}
-                    className={"reroll"}
-                />
+                <section className="characterButtonSection">
+                    {determineBackButton()}
+                    <OptionButton
+                        label={"GIMME ANUDDER MIN!"}
+                        onClick={fetchCharacter}
+                        value={""}
+                        id={"reroll"}
+                        containerName={"inline-block grid-center"}
+                        className={"reroll"}
+                    />
+                    {determineForwardButton()}
+                </section>
+                <h5 className="characterCount">{characterIndex+1}/{history.length}</h5>
                 <section className="characterContainer">
-                    <h1 className="characterName">{character.name}</h1>
+                    <h1 className="characterName">{history[characterIndex].name}</h1>
                     <h2 className="characterDescriptor">Level&nbsp;
-                        {character.level}&nbsp;
-                        {character.race}&nbsp;
-                        {character.class}
+                        {history[characterIndex].level}&nbsp;
+                        {history[characterIndex].race}&nbsp;
+                        {history[characterIndex].class}
                     </h2>
                     <section className="threeColumn">
                         <div className="thirdColumn">
                             <Attributes
-                                attributes={character.stats}
+                                attributes={history[characterIndex].stats}
                             />
                             <hr/>
                             <Proficiencies
-                                proficiencies={character.proficiency["Other"]}
+                                proficiencies={history[characterIndex].proficiency["Other"]}
                             />
                         </div>
                         <div className="thirdColumn">
                             <CombatProperties
-                                hp={character.hitpoints}
-                                ac={character.armorclass}
-                                hd={character.hitdice}
+                                hp={history[characterIndex].hitpoints}
+                                ac={history[characterIndex].armorclass}
+                                hd={history[characterIndex].hitdice}
                             />
                             <hr/>
                             <SavingThrows
-                                prof={character.proficiency["Proficient Throws"]}
-                                saves={character.proficiency["Saving Throws"]}
+                                prof={history[characterIndex].proficiency["Proficient Throws"]}
+                                saves={history[characterIndex].proficiency["Saving Throws"]}
                             />
                             <hr/>
                             <Skills
-                                skills={character.proficiency}
+                                skills={history[characterIndex].proficiency}
                             />
                             <hr/>
                             <Equipment
-                                equipment={character.equipment}
+                                equipment={history[characterIndex].equipment}
                             />
                         </div>
                         <div className="thirdColumn">
                             <Background
-                                background={character.background}
+                                background={history[characterIndex].background}
                             />
                             <hr/>
                             <Features
-                                features={character.features}
+                                features={history[characterIndex].features}
                             />
                         </div>
                     </section>
